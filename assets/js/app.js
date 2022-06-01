4578445c4edf790f5751d1e0ee398a43813081e5
ughurba <ughurba@edu.code.az>
2022-06-01 17:52:10 +0400

    writed functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,147 @@ const formLoqin = document.querySelector('.form-loqin');
 const btnLogout = document.querySelector('.btn-logout');
 
 
+const userGoodList = JSON.parse(localStorage.getItem('users'));
 
+userGoodList.forEach(user => {
+    if (user.isLoqin) {
+        profileLogin(user.username);
+    }
+})
+
+
+function renderDisplay(item, display) {
+    item.style.display = display;
+}
+
+function addtoUserInfo(userName, password) {
+
+    let userList = JSON.parse(localStorage.getItem("users")) || [];
+
+    userList.push({
+        isLoqin: false,
+        username: userName,
+        password: password,
+    });
+
+    localStorage.setItem("users", JSON.stringify(userList));
+}
+
+function registrationHandler(ev) {
+    ev.preventDefault();
+
+    renderDisplay(registrWindow, "block");
+    renderDisplay(overlayWindow, "block");
+}
+
+function renderInputValue(value) {
+    userNameRegistrInput.value = value;
+    passwordRegistrInput.value = value;
+}
+
+function checkUserInfoRegistr(userName, password) {
+    userList = JSON.parse(localStorage.getItem("users"));
+
+    if (userList == null) {
+        addtoUserInfo(userName, password);
+    }
+
+
+    let element = userList.find(
+        (user) => user.userName === userName || user.password === password
+    );
+
+
+    if (element === undefined) {
+
+        addtoUserInfo(userName, password);
+        renderInputValue('')
+
+    } else {
+        alert("bad");
+        renderInputValue('')
+
+    }
+}
+
+function saveUserRegistrHandler(ev) {
+    ev.preventDefault();
+
+    let userNameRegistrValue = userNameRegistrInput.value;
+    let passwordRegistrValue = passwordRegistrInput.value;
+
+    checkUserInfoRegistr(userNameRegistrValue, passwordRegistrValue);
+}
+
+
+function profileLogin(userName) {
+
+    renderDisplay(formLoqin, "none");
+    let h1 = document.getElementById('title-page')
+    h1.innerText = `hello ${userName} how are you?`;
+    renderDisplay(page, "block");
+    const userGoodList = JSON.parse(localStorage.getItem('users'));
+
+
+    localStorage.setItem('users', JSON.stringify(userGoodList))
+
+
+}
+
+function wayOutInProfile() {
+    renderDisplay(page, 'none');
+    renderDisplay(formLoqin, "flex");
+
+
+    let userGoodList = JSON.parse(localStorage.getItem('users'));
+    userGoodList.forEach(user => {
+        user.isLoqin = false
+
+    });
+
+    localStorage.setItem('users', JSON.stringify(userGoodList));
+
+}
+
+function checkUserInfoLoqin(userName, password) {
+
+    let userList = JSON.parse(localStorage.getItem("users"));
+
+    let element = userList.find(
+        (user) => user.username === userName || user.password === password
+    );
+
+
+    if (element === undefined) {
+
+        alert('bele bir account yoxdur')
+
+    } else {
+        const selectedCheckBox = document.querySelector('.form-check-input').checked
+
+        if (selectedCheckBox) {
+            element.checkUser = true
+        }
+
+
+        element.isLoqin = true;
+        localStorage.setItem('users', JSON.stringify(userList))
+        profileLogin(element.username)
+
+    }
+
+
+}
+
+
+function loqinUserHandler(ev) {
+    ev.preventDefault();
+
+    let userNameLoqinInputValue = userNameLoqinInput.value;
+    let passwordLoqinInputValue = passwordLoqunInput.value;
+    checkUserInfoLoqin(userNameLoqinInputValue, passwordLoqinInputValue)
+
+}
 
 
 
